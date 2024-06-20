c9e31c601c630739f8b0edc8301ab120debdb47d
Sohan-Patra3 <sohanpatra3@gmail.com>
2024-06-20 19:54:24 +0530

    day-1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\admin\adminLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login' , [adminLoginController::class , 'index'])->name('admin.login');
